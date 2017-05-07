b2897b9e0285321e5fe5b9128fb38a8eab8bbcf6
Александр <Z80@iMac-Aleksandr.local>
2017-05-07 16:16:09 +0300

    add id props to path, add comments

--- src/index.js
+++ src/index.js
@@ -1,67 +1,101 @@
-/* @jsx Kek.d */
+function K() {}
 
-function Kek() {}
-Kek.nodes = {};
-Kek.rNodes = [];
+/**
+ * All Nodes list
+ */
+K.n = {};
 
-Kek.d = function(name, props) {
-    return {n: name, p: props, c: Array.prototype.slice.call(arguments, 2)};
-}
+/**
+ * Painted nodes
+ */
+K.r = [];
 
-Kek.redraw = function(node, component, path) {
-    Kek.rNodes = [];
-    Kek.append(node, component, path);
-    var keys = Object.keys(Kek.nodes);
-    if (keys.length === Kek.rNodes) return;
+/**
+ * JSX DOM
+ * @param node 
+ * @param props
+ */
+K.d = function(name, props) {
+  return {n: name, p: props, c: Array.prototype.slice.call(arguments, 2)};
+};
 
-    keys.forEach(function(name) {
-        if (Kek.rNodes.indexOf(name) === -1) {
-            Kek.nodes[name].remove();
-            delete Kek.nodes[name];
-        }
-    })
-}
-Kek._each = function(node, childs, path) {
-    if (!(childs instanceof Array)) return;
-    childs.forEach(function(child, i) {
-        Kek.append(node, child, path + i + '.');
-    });
-}
-Kek.append = function(node, component, path) {
-    Kek._each(node, component, path);
+/**
+ * Redrawn dom
+ * @param node 
+ * @param component
+ * @param path
+ */
+K.redraw = function(node, component, path) {
+  K.r = [];
+  K.a(node, component, path);
+  var keys = Object.keys(K.n);  // nodes
+  if (keys.length === K.r) return;
 
-    if (!Kek.node) {
-        Kek.node = node;
-        Kek.root = component;
+  keys.forEach(function(name) {
+    if (K.r.indexOf(name) === -1) {
+      K.n[name].remove();  // remove node
+      delete K.n[name];  // 
     }
+  });
+};
 
-    node = node || Kek.node;
-    component = component || Kek.root;
-    path = path || '0.';
-    var name = component.n;
-    var props = component.p;
-    var childs = component.c;
+/**
+ * Each childs
+ * @param node 
+ * @param childs
+ * @param path
+ */
+K.e = function(node, childs, path) {
+  if (!(childs instanceof Array)) return;
+  childs.forEach(function(child, i) {
+    K.a(node, child, path + (!child.p || child.p.id) + i + '.');
+  });
+};
+
+/** 
+ * Append to dom node
+ * @param node 
+ * @param component
+ * @param path
+*/
+K.a = function(node, component, path) {
+  K.e(node, component, path);
+
+  if (!K.node) { 
+    K.node = node;
+    K.root = component;
+  }
+
+  node = node || K.node;
+  component = component || K.root;
+  path = path || '0.';
+  var name = component.n;
+  var props = component.p;
+  var childs = component.c;
     
-    if (typeof component === 'boolean') return;
+  if (typeof component === 'boolean') return;
 
-    Kek.rNodes.push(path);
+  K.r.push(path);
 
-    if (typeof name === 'function') {
-        return Kek.append(node, (new name(props, childs)).render(), path);
-    }
+  if (typeof name === 'function') {
+    return K.a(node, (new name(props, childs)).render(), path);
+  }
 
-    if (typeof component === 'string') {
-        return node.innerText = component;
-    }
+  if (typeof component === 'string') {
+    return node.innerText = component;
+  }
 
-    var ref = Kek.nodes[path] || document.createElement(name);
+  var ref = K.n[path] || document.createElement(name);
 
-    if(props) {
-        Object.keys(props).forEach(function(name) {
-            if (ref[name] !== props[name]) ref[name] = props[name];
-        });
-    }
+  if(props) {
+    Object.keys(props).forEach(function(name) {
+      if (ref[name] !== props[name]) ref[name] = props[name];
+    });
+  }
 
-    node.appendChild(Kek.nodes[path] = ref);
-    Kek._each(ref, childs, path);
-}
+  node.appendChild(K.n[path] = ref);
+  K.e(ref, childs, path);
+};
+
+K.append = K.a;
+var Kek = K; // eslint-disable-line
